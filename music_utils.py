893a3432e6b748ab502d1170be82bd035abfa7c3
log5r <jusk1nsnt9gi>
2023-02-18 14:41:00 +0900

    サフィックスの有無を判断する必要がありそうだったので分解

--- music_utils.py
+++ music_utils.py
@@ -1,13 +1,19 @@
 # コードのサフィックスを除外
 def remove_chord_suffix(chord_string):
     lst = list(chord_string)
-    if len(lst) <= 2:
-        # サフィックスがないなら処理しない
-        if len(lst) <= 1:
-            return chord_string
-        if len(lst) == 2 and lst[1] == "m":
-            return chord_string
+    if not chord_has_suffix(chord_string):
+        return chord_string
     if lst[1] == "m" and lst[2] != "a":
         return lst[0] + lst[1]
     else:
         return lst[0]
+
+
+def chord_has_suffix(chord_string):
+    lst = list(chord_string)
+    if len(lst) <= 2:
+        if len(lst) <= 1:
+            return False
+        if len(lst) == 2 and lst[1] == "m":
+            return False
+    return True
