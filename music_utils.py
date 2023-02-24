8377fc15d14d8425b211acfe8a2f5ee49c9026bc
log5r <jusk1nsnt9gi>
2023-02-24 17:25:04 +0900

    補正ロジック更新

--- music_utils.py
+++ music_utils.py
@@ -79,7 +79,7 @@ def note_chain_breaking_condition(i):
 
 # ノート補正
 def corrected_note_num_list(notenumlist, chord_prog, remove_suffix_prob, strict_mode):
-    fixed_note_num_list = []
+    res_note_list = []
     same_note_chain = 0
     through_count = 0
     bottom_count = 0
@@ -94,56 +94,80 @@ def corrected_note_num_list(notenumlist, chord_prog, remove_suffix_prob, strict_
         fixed_note = e
         target_class = fixed_note % 12
 
+        good_notes = goal_chord.pitchClasses
+
+        # --- 攻めの補正 ---
+
+        # 5小節目の大胆な変更は必要？
+        will_fix_bar_5 = False
+        if 64 <= i < 76 and i % 4 == 0:
+            if random.random() < 0.45:
+                will_fix_bar_5 = True
+            else:
+                will_fix_bar_5 = False
+
         if i == 0:
             if random.random() < 0.6:
-                fixed_note = 60 + goal_chord.pitchClasses[-1]
+                fixed_note = 60 + good_notes[-1]
             else:
-                fixed_note = 60 + random.choice(goal_chord.pitchClasses)
-        elif fixed_note < 24:
-            # 低すぎる音が出たら適宜取り繕う
-            next_n_idx = bottom_count % len(goal_chord.pitchClasses)
-            if len(fixed_note_num_list) > 0:
-                if random.random() < 0.5:
-                    fixed_note = fixed_note_num_list[-1]
-                else:
-                    fixed_note = (fixed_note_num_list[-1] // 12) * 12 + goal_chord.pitchClasses[next_n_idx]
-                    bottom_count += 1
-        elif (fixed_note % 12) not in goal_chord.pitchClasses:
-            # コード構成音にない音について、特定条件のもとで補正する
-            if any(note_canonicalization_conditions(i)):
-                clist = []
-                for k in goal_chord:
-                    expected_class = k.pitch.midi % 12
-                    buf = expected_class - target_class
-                    clist.append([abs(buf), buf])
-                clist.sort(key=lambda z: z[0])
-                fixed_note = fixed_note + clist[0][1]
+                fixed_note = 60 + random.choice(good_notes)
+        elif 1 <= i < 3:
+            fixed_note = res_note_list[-1]
+        elif 64 <= i < 76 and will_fix_bar_5:
+            pnb = ((6 + res_note_list[-1]) // 12) * 12
+            fixed_note = [pnb + good_notes[-2], pnb + good_notes[-1], pnb + good_notes[-1] + 2][i % 3]
+        else:
+            # --- 守りの補正 ---
+            if fixed_note < 24:
+                # 低すぎる音が出たら適宜取り繕う
+                next_n_idx = bottom_count % len(good_notes)
+                if len(res_note_list) > 0:
+                    if random.random() < 0.5:
+                        fixed_note = res_note_list[-1]
+                    else:
+                        fixed_note = ((6 + res_note_list[-1]) // 12) * 12 + good_notes[next_n_idx]
+                        bottom_count += 1
+            elif (fixed_note % 12) not in good_notes:
+                # コード構成音にない音について、特定条件のもとで補正する
+                if any(note_canonicalization_conditions(i)):
+                    clist = []
+                    for k in goal_chord:
+                        expected_class = k.pitch.midi % 12
+                        buf = expected_class - target_class
+                        clist.append([abs(buf), buf])
+                    clist.sort(key=lambda z: z[0])
+                    fixed_note = fixed_note + clist[0][1]
 
-        # C or A で黒鍵は登場しないはずなので、補正する。ただし、サフィックス付きコードはたまに許す
-        if i != 0 and (fixed_note % 12) in [1, 3, 6, 8, 10]:
-            if strict_mode or (through_count % 5 != 1 and not chord_has_suffix(goal_chord.figure)):
-                through_count += 1
-                fixed_note += random.choice([1, -1])
+            # C or A で黒鍵は登場しないはずなので、補正する。ただし、サフィックス付きコードはたまに許す
+            if i != 0 and (fixed_note % 12) in [1, 3, 6, 8, 10]:
+                if strict_mode or (through_count % 5 != 1 and not chord_has_suffix(goal_chord.figure)):
+                    through_count += 1
+                    fixed_note += random.choice([1, -1])
 
-        # 同一ノートが連続したときの処理
-        if i != 0 and len(fixed_note_num_list) > 0 and fixed_note_num_list[-1] == fixed_note:
-            same_note_chain += 1
-            if any(note_chain_breaking_condition(i)):
-                err_note_class = [fixed_note_num_list[-1] % 12]
-                if same_note_chain % 4 != 1 and len(fixed_note_num_list) > 1:
-                    err_note_class += [fixed_note_num_list[-2] % 12]
-                    if len(fixed_note_num_list) > 2 and len(goal_chord.pitchClasses) > 3:
-                        err_note_class += [fixed_note_num_list[-3] % 12]
-                delta = random.choice(list(filter(lambda nt: nt not in err_note_class, goal_chord.pitchClasses)))
-                fixed_note = (fixed_note // 12) * 12 + delta
+            # 同一ノートが連続したときの処理
+            if i != 0 and len(res_note_list) > 0 and res_note_list[-1] == fixed_note:
+                same_note_chain += 1
+                if any(note_chain_breaking_condition(i)):
+                    err_note_class = [res_note_list[-1] % 12]
+                    if same_note_chain % 4 != 1 and len(res_note_list) > 1:
+                        err_note_class += [res_note_list[-2] % 12]
+                        if len(res_note_list) > 2 and len(good_notes) > 3:
+                            err_note_class += [res_note_list[-3] % 12]
+                    delta = random.choice(list(filter(lambda nt: nt not in err_note_class, good_notes)))
+                    fixed_note = (fixed_note // 12) * 12 + delta
 
-        fixed_note_num_list.append(fixed_note)
+        # 高すぎる音は結構耳につくので引いておく
+        if fixed_note > 96:
+            fixed_note -= 12
+        res_note_list.append(fixed_note)
 
     # 最後の小節
-    end_note_scale = (fixed_note_num_list[-1] // 12) * 12
+    end_note_scale = (res_note_list[-1] // 12) * 12
     for cls in chord_prog[-1].pitchClasses:
-        fixed_note_num_list.append(end_note_scale + cls)
-    fixed_note_num_list.append(end_note_scale + 12 + chord_prog[-1].pitchClasses[0])
-    fixed_note_num_list += [fixed_note_num_list[-1]] * 8
+        res_note_list.append(end_note_scale + cls)
+    res_note_list.append(end_note_scale + 12 + chord_prog[-1].pitchClasses[0])
+    res_note_list += [res_note_list[-1]] * 8
+
+    return res_note_list
 
     return fixed_note_num_list
